ede03504eb5b8573b6b1518ee868c4adc145ade7
undefined <undefined>
2021-12-09 17:13:22 +0800

    修改样式

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 import psList from "ps-list";
 import chalk from "chalk";
 import "console.table";
-const MAX_OVERFLOAD = 80;
+const MAX_OVERFLOAD = 20;
 const detectOverload = async () => {
   let processes = await psList();
   const overloadProcessed = [];
   processes.sort((a, b) => {
     return a.cpu - b.cpu > 0 ? -1 : 1;
   });
-  processes.forEach((process) => {
-    if (process.cpu > MAX_OVERFLOAD) {
-      overloadProcessed.push(process);
-    }
+  const table = [];
+  processes.slice(0, 10).forEach((process) => {
+    table.push(getTableCol(process));
   });
+  let hasOverload = false;
   console.clear();
-  //   console.log(overloadProcessed);
-  if (overloadProcessed.length > 0) {
-    console.log(chalk.red("Overload detected"));
-    const table = [];
-    overloadProcessed.forEach((process) => {
-      const name = process.cmd
-        .replace(/^.*?\/([^/]*$)/, "$1")
-        .replace(/(.*?) .*/, "$1");
 
-      table.push({
-        name: chalk.green(name.padEnd(20)),
-        pid: chalk.blue(String(process.pid).padEnd(10)),
-        cpu: `${chalk.red(process.cpu)}%`.padEnd(10),
-        memery: `${chalk.red(process.memory)}%`.padEnd(10),
-      });
-    });
-    console.table(table);
-  } else {
-    console.log(chalk.green("No overload detected"));
-    const table = [];
-    processes.slice(0, 5).forEach((process) => {
-      const name = process.cmd
-        .replace(/^.*?\/([^/]*$)/, "$1")
-        .replace(/(.*?) .*/, "$1");
-      table.push({
-        name: chalk.green(name.padEnd(20)),
-        pid: chalk.green(String(process.pid).padEnd(10)),
-        cpu: `${chalk.green(process.cpu)}%`.padEnd(10),
-        memery: `${chalk.green(process.memory)}%`.padEnd(10),
-      });
-    });
-    console.table(table);
-  }
+  // //   console.log(overloadProcessed);
+  // if (overloadProcessed.length > 0) {
+  //   console.log(chalk.red("Overload detected"));
+
+  //   console.table(table);
+  // } else {
+  //   console.log(chalk.green("No overload detected"));
+
+  console.table(table);
+  // }
 };
 
 setInterval(detectOverload, 1000);
+
+function getTableCol(process) {
+  const name = process.cmd
+    .replace(/^.*?\/([^/]*$)/, "$1")
+    .replace(/(.*?) .*/, "$1");
+  if (process.cpu < MAX_OVERFLOAD) {
+    return {
+      name: chalk.green(name.padEnd(20).substr(0, 20)),
+      pid: chalk.green(String(process.pid).padEnd(10)),
+      cpu: chalk.white.bgGreen.bold((process.cpu + "%").padStart(8)),
+      memery: chalk.white.bgGreen.bold((process.memory + "%").padStart(8)),
+    };
+  } else {
+    return {
+      name: chalk.red(name.padEnd(20).substr(0, 20)),
+      pid: chalk.red(String(process.pid).padEnd(10)),
+      cpu: chalk.white.bgRed.bold((process.cpu + "%").padStart(8)),
+      memery: chalk.white.bgRed.bold((process.memory + "%").padStart(8)),
+    };
+  }
+}
